be96c7423a89ad2f977a0a7c240cb379bee51538
Yu-sooho <souho456@naver.com>
2023-11-15 17:07:19 +0900

    Feat: add naver login sdk

--- android/app/src/main/kotlin/com/guardian/gymCalendar/MainActivity.kt
+++ android/app/src/main/kotlin/com/guardian/gymCalendar/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.guardian.gymCalendar
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
